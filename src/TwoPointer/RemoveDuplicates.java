89d322ae0354a004ed87b40cfaa9aedbc1de6d45
Yogesh Kanwar <yogeshkanwar2989@gmail.com>
2026-02-25 22:16:53 +0530

    remove duplicates

---
+++ src/TwoPointer/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package TwoPointer;
+
+public class RemoveDuplicates {
+
+    public static int remove(int[] arr){
+        if(arr.length <= 1) {
+            return arr.length;
+        }
+        int nextNonDuplicate = 1;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] != arr[nextNonDuplicate - 1]){
+                arr[nextNonDuplicate] = arr[i];
+                nextNonDuplicate ++;c
+            }
+        }
+        return nextNonDuplicate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 3, 3, 6, 9, 9};
+        int length1 = remove(arr1);
+        System.out.println(length1); // 4
+
+        int[] arr2 = {2, 2, 2, 11};
+        int length2 = remove(arr2);
+        System.out.println(length2);    }
+}
