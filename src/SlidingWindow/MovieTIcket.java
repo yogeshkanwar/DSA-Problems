23331dcab6057022888298ae354affda01cc544f
Yogesh Kanwar <yogeshkanwar2989@gmail.com>
2026-02-27 23:00:31 +0530

    refactor some changes

--- src/SlidingWindow/MovieTIcket.java
+++ src/SlidingWindow/MovieTIcket.java
@@ -3,8 +3,7 @@ package SlidingWindow;
 
 public class MovieTIcket {
 
-    public static void findMinRows(int arr[],int k){
-
+    public static int findMinRows(int arr[],int k){
 
         int minLength = arr.length;
         int start = 0;
@@ -21,16 +20,17 @@ public class MovieTIcket {
             }
         }
         if(!exist){
-            System.out.println("Match not found");
+            return -1;
         }
-        System.out.println(minLength);
+        return minLength;
     }
 
     public static void main(String[] args) {
         int arr[] = {5, 3, 4, 8, 2}; int k=11;
-        findMinRows(arr,k);
+        int n = findMinRows(arr,k);
+        System.out.println(n);
         int arr1[] = {2,1,2,1}; int k1=8;
-        findMinRows(arr1,k1);
-
+        int n1 = findMinRows(arr1,k1);
+        System.out.println(n1);
     }
 }
